fafed1f36f1f729202b3a3ebc6d4c36f07afc1b3
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL>
2024-09-27 16:18:42 +0530

    Update script.js with new content

--- script.js
+++ script.js
@@ -18,5 +18,32 @@ divs.forEach((div) => {
     });
 });
 
+let button = document.querySelector("button");
+button.addEventListener("click", () => {
+  let boxs = parseInt(prompt("how many squares per side?"));
+  if (boxs > 100) {
+    alert("too big");
+  } else {
+    container.innerHTML = "";
+    for (let i = 1; i <= boxs; i++) {
+      for (let j = 1; j <= boxs; j++) {
+        let num = document.createElement("p");
+        let div = document.createElement("div");
+        div.style.width = 800 / boxs + "px";
+        div.style.height = 800 / boxs + "px";
+        div.appendChild(num);
+        container.appendChild(div);
+      }
+    }
+    let divs = document.querySelectorAll(".container div");
+
+    divs.forEach((div) => {
+      div.addEventListener("mouseover", () => {
+        div.style.backgroundColor = "pink";
+      });
+    });
+  }
+});
+
 
 
