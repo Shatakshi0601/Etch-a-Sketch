2aad261e576b837d8b438ee3668145399432d21f
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL>
2024-09-27 15:01:08 +0530

    Update script.js with new content

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 let container= document.querySelector(".container");
 
+for (let i = 1; i<=16; i++); {
+    for( let j=1; j<=16; j++); {
+        let num = document.createElement("p");
+        let div = document.createElement("div");
+        div.style.width = 800/16 + "px";
+        div.style.height= 800/16 + "px";
+        div.appendChild(num);
+        container.appendChild(div);
+    }
+}
+
+let divs = querySelectorAll(".container div");
+
+
 
