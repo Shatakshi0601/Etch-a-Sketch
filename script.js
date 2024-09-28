737857b67d200265479cd37e35e23fba0b14c98f
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL>
2024-09-28 10:42:45 +0530

    Update script.js with new content

--- script.js
+++ script.js
@@ -1,45 +1,33 @@
-let container= document.querySelector(".container");
+let container = document.querySelector(".container");
 
-for (let i = 1; i<=16; i++) {
-    for( let j=1; j<=16; j++) {
-        let div = document.createElement("div");
-        div.style.width = 800/16 + "px";
-        div.style.height= 800/16 + "px";
-        container.appendChild(div);
+// Function to create grid squares
+function createGrid(boxes) {
+    container.innerHTML = ""; // Clear existing grid
+    for (let i = 0; i < boxes; i++) {
+        for (let j = 0; j < boxes; j++) {
+            let div = document.createElement("div");
+            div.classList.add("grid-square");
+            container.appendChild(div);
+        }
     }
 }
 
-let divs = document.querySelectorAll(".container div");
-divs.forEach((div) => {
-    div.addEventListener("mouseover", () => {
-        div.style.backgroundColor = "pink";
-    });
+// Initial grid creation
+createGrid(16);
+
+// Event delegation for mouseover effect
+container.addEventListener("mouseover", (event) => {
+    if (event.target.classList.contains("grid-square")) {
+        event.target.style.backgroundColor = "pink";
+    }
 });
 
 let button = document.querySelector("button");
 button.addEventListener("click", () => {
-  let boxes = parseInt(prompt("how many squares per side?"));
-  if (boxes > 100) {
-    alert("too big");
-  } else {
-    container.innerHTML = "";
-    for (let i = 1; i <= boxes; i++) {
-      for (let j = 1; j <= boxes; j++) {
-        let div = document.createElement("div");
-        div.style.width = 800 / boxes + "px";
-        div.style.height = 800 / boxes + "px";
-        container.appendChild(div);
-      }
+    let boxes = parseInt(prompt("How many squares per side?"));
+    if (boxes > 100) {
+        alert("Too big!");
+    } else {
+        createGrid(boxes);
     }
-    let divs = document.querySelectorAll(".container div");
-
-    divs.forEach((div) => {
-      div.addEventListener("mouseover", () => {
-        div.style.backgroundColor = "pink";
-      });
-    });
-  }
 });
-
-
-
