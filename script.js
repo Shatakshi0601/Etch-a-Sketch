3ef291af50546fab6fb3f663c12770e583e9725a
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL>
2024-09-28 11:59:37 +0530

    Update script.js with new content

--- script.js
+++ script.js
@@ -1,51 +1,37 @@
 let container = document.querySelector(".container");
 
-for (let i = 1; i <= 16; i++) {
-  for (let j = 1; j <= 16; j++) {
-    let num = document.createElement("p");
-    let div = document.createElement("div");
-    div.style.width = 800 / 16 + "px";
-    div.style.height = 800 / 16 + "px";
-    div.appendChild(num);
-    container.appendChild(div);
-  }
+
+function createGrid(boxes) {
+    container.innerHTML = ""; 
+    container.style.gridTemplateColumns = `repeat(${boxes}, 1fr)`; 
+    for (let i = 0; i < boxes; i++) {
+        for (let j = 0; j < boxes; j++) {
+            let div = document.createElement("div");
+            let num = document.createElement("p");
+            num.textContent = `${i * boxes + j + 1}`; 
+            div.classList.add("grid-square");
+            div.appendChild(num);
+            container.appendChild(div);
+        }
+    }
 }
 
 
-let divs = document.querySelectorAll(".container div");
-divs.forEach((div) => {
-  div.addEventListener("mouseover", () => {
-    div.style.backgroundColor = "pink";
-  });
-});
+createGrid(16);
 
+
+container.addEventListener("mouseover", (event) => {
+    if (event.target.classList.contains("grid-square")) {
+        event.target.style.backgroundColor = "pink";
+    }
+});
 
 let button = document.querySelector("button");
 button.addEventListener("click", () => {
-  let boxes = parseInt(prompt("how many squares per side?"));
-  if (boxes > 100) {
-    alert("too big");
-  } else {
-    container.innerHTML = "";
-    container.style.gridTemplateColumns = `repeat(${boxes}, 1fr)`;
-    for (let i = 1; i <= boxes; i++) {
-      for (let j = 1; j <= boxes; j++) {
-        let num = document.createElement("p");
-        let div = document.createElement("div");
-        num.textContent = `${i * boxes + j + 1}`; 
-        div.classList.add("grid-square");
-        div.style.width = 800 / boxes + "px";
-        div.style.height = 800 / boxes + "px";
-        div.appendChild(num);
-        container.appendChild(div);
-      }
+    let boxes = parseInt(prompt("How many squares per side?"));
+    if (boxes > 100) {
+        alert("Too big!");
+    } else {
+        createGrid(boxes);
     }
-    let divs = document.querySelectorAll(".container div");
-
-    divs.forEach((div) => {
-      div.addEventListener("mouseover", () => {
-        div.style.backgroundColor = "pink";
-      });
-    });
-  }
 });
